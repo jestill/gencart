f1a5648aaf1de551801acd292cbb7efe6114f7f2
James Estill <JamesEstill@gmail.com>
2009-08-11 16:15:29 +0000

    Updated ArabJoinCount.R -Only showing test for chromosome 5, revision 5

--- R/ArabJoinCount.R
+++ R/ArabJoinCount.R
@@ -1,48 +1,36 @@
-# This will be where I am working on using the spdep library to
-# do join count tests of contiguity of +/- from genomic data sets
-# For prokaryotic datasets this will need to modified to use the
-# mapping onto a torus to avoid edge effects
-
+#-----------------------------------------------------------+
+# join_count_test.r - Test join count statistics            |
+#-----------------------------------------------------------+
+#  AUTHOR: James C. Estill                                  |
+# CONTACT: JamesEstill at gmail.com                         |
+# STARTED: 07/2005                                          |
+# UPDATED: 08/11/2009                                       |
+# DESCRIPTION:                                              |
+# Test using the spdep library to do join count tests of    |
+# contiguity of the +/- from genomic data sets.             |
+# For prokaryotic datasets, this could be modified to use   |
+# the mapping onto a torus to avoid edge effects.           |
+#                                                           |
+#-----------------------------------------------------------+
 # For the following to work the data MUST be listed in the proper order (ie. presort the list by midpoint before doing the analysis)
 # LOAD THE SPDEP LIBRARY
 library(spdep)
 
-
-
-InputFile = "C:/RData/Arab/ArabStrandr5chr5.txt"
-GeneData <- read.table(InputFile, header=T)
-numgenes = (length(GeneData[[1]]))                                # Determine the number of genes for drawing the chromosome nb
-GeneData <- GeneData[sort.list(GeneData$MidPoint),]               # Do A sort to get the data in the correct order
-chrom.nb <- cell2nb( numgenes, 1, type="rook", torus=FALSE)
-Result5 <-  joincount.multi(GeneData$Strand, nb2listw(chrom.nb, style="B"))                 # This may work if the GeneData$Strand is automatically used as a factor data type
-
-
-InputFile = "C:/RData/Arab/ArabStrandr5chr4.txt"
-GeneData <- read.table(InputFile, header=T)
-numgenes = (length(GeneData[[1]]))                                # Determine the number of genes for drawing the chromosome nb
-GeneData <- GeneData[sort.list(GeneData$MidPoint),]               # Do A sort to get the data in the correct order
-chrom.nb <- cell2nb( numgenes, 1, type="rook", torus=FALSE)
-Result4 <-  joincount.multi(GeneData$Strand, nb2listw(chrom.nb, style="B"))                 # This may work if the GeneData$Strand is automatically used as a factor data type
-
-InputFile = "C:/RData/Arab/ArabStrandr5chr3.txt"
-GeneData <- read.table(InputFile, header=T)
-numgenes = (length(GeneData[[1]]))                                # Determine the number of genes for drawing the chromosome nb
-GeneData <- GeneData[sort.list(GeneData$MidPoint),]               # Do A sort to get the data in the correct order
-chrom.nb <- cell2nb( numgenes, 1, type="rook", torus=FALSE)
-Result3 <-  joincount.multi(GeneData$Strand, nb2listw(chrom.nb, style="B"))                 # This may work if the GeneData$Strand is automatically used as a factor data type
-
-InputFile = "C:/RData/Arab/ArabStrandr5chr2.txt"
-GeneData <- read.table(InputFile, header=T)
-numgenes = (length(GeneData[[1]]))                                # Determine the number of genes for drawing the chromosome nb
-GeneData <- GeneData[sort.list(GeneData$MidPoint),]               # Do A sort to get the data in the correct order
-chrom.nb <- cell2nb( numgenes, 1, type="rook", torus=FALSE)
-Result2 <-  joincount.multi(GeneData$Strand, nb2listw(chrom.nb, style="B"))                 # This may work if the GeneData$Strand is automatically used as a factor data type
-
-InputFile = "C:/RData/Arab/ArabStrandr5chr1.txt"
-GeneData <- read.table(InputFile, header=T)
-numgenes = (length(GeneData[[1]]))                                # Determine the number of genes for drawing the chromosome nb
-GeneData <- GeneData[sort.list(GeneData$MidPoint),]               # Do A sort to get the data in the correct order
-chrom.nb <- cell2nb( numgenes, 1, type="rook", torus=FALSE)
-Result1 <-  joincount.multi(GeneData$Strand, nb2listw(chrom.nb, style="B"))                 # This may work if the GeneData$Strand is automatically used as a factor data type
-
-
+#-----------------------------+
+# ROOKS CASE JOIN COUNT       |
+# INPUT TEXT FILE             |
+#-----------------------------+
+# Input is Arabidopsis chromosome 5
+# This is OLD data from TIGR version 5 of the arabidopsis 
+InputFile = "../data/ArabStrandr5chr5.txt";
+# Import data to the GeneData object
+GeneData <- read.table(InputFile, header=T);
+# Determine the number of genes for drawing the chromosome nb
+numgenes = (length(GeneData[[1]]));
+# Do A sort to get the data in the correct order
+# Treat each locus as the midpoint
+GeneData <- GeneData[sort.list(GeneData$MidPoint),];
+# Generate the neighbors files
+chrom.nb <- cell2nb( numgenes, 1, type="rook", torus=FALSE);
+# This may work if the GeneData$Strand is automatically used as a factor data type
+Result5 <-  joincount.multi(GeneData$Strand, nb2listw(chrom.nb, style="B"));
